24b659bcdb07e2cd58d08bacc6b8258a4ceafda6
idesuga <2472489669@qq.com>
2024-03-19 20:13:20 +0800

    '2024.3.19'

---
+++ src/main/java/com/chat/chatpro/ChatProApplication.java
@@ -0,0 +1,13 @@
+package com.chat.chatpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatProApplication.class, args);
+    }
+
+}
